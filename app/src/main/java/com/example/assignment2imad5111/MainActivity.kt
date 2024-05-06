35ea5935b250f03ee7ea92740626b554548ceb60
OliverDixon5 <ST10438499@vcconnect.edu.za>
2024-05-06 13:21:44 +0200

    Added code to send user to next screen -problem with the activity_main2.xml not being recognised -attempted to import plugins but there where out of date and didn't work -added apply(plugin = "kotlin-parcelize") which didn't help -more research needed for fix Renamed Id's for the Buttons TextViews and Image Views Set a placeholder image Set Buttons that would change the placeholder based on user input Added a counter if the buttons where pressed TextViews would change corresponding to the users button presses

--- app/src/main/java/com/example/assignment2imad5111/MainActivity.kt
+++ app/src/main/java/com/example/assignment2imad5111/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.assignment2imad5111
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val goToActivityMain2Button = findViewById<Button>(R.id.goToActivityMain2Button)
+
+        goToActivityMain2Button.setOnClickListener {
+            //val intentGoToActivityMain2Button = Intent(this, activitymain::class.java)
+            //startActivity(intentGoToActivityMain2Button)
+
+        }
     }
 }
+
--- app/src/main/java/com/example/assignment2imad5111/MainActivity2.kt
+++ app/src/main/java/com/example/assignment2imad5111/MainActivity2.kt
@@ -1,11 +1,52 @@
 package com.example.assignment2imad5111
 
-import androidx.appcompat.app.AppCompatActivity
+
+
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity2 : AppCompatActivity() {
+
+    private var clickHungerCount = 0
+    private var clickHappyCount = 0
+    private var clickWashingCount = 0
+    @SuppressLint("MissingInflatedId", "CutPasteId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
+
+        val placeholderImage = findViewById<ImageView>(R.id.placeholderImage)
+
+        val eatingButton = findViewById<Button>(R.id.playButton)
+        val playingButton = findViewById<Button>(R.id.feedButton)
+        val washingButton = findViewById<Button>(R.id.cleanButton)
+
+        val hungerCount = findViewById<TextView>(R.id.hungerCount)
+        val happyCount = findViewById<TextView>(R.id.hungerCount)
+        val washingCount = findViewById<TextView>(R.id.hungerCount)
+
+        placeholderImage.setImageResource(R.drawable.snoopy_png95)
+
+        eatingButton.setOnClickListener {
+            placeholderImage.setImageResource(R.drawable.playingimage1)
+            clickHungerCount++
+            hungerCount.text = clickHungerCount.toString()
+        }
+        playingButton.setOnClickListener {
+            placeholderImage.setImageResource(R.drawable.eatingimage1)
+            clickHappyCount++
+            happyCount.text = clickHappyCount.toString()
+        }
+        washingButton.setOnClickListener {
+            placeholderImage.setImageResource(R.drawable.washingimage1)
+            clickWashingCount++
+            washingCount.text = clickWashingCount.toString()
+        }
+
     }
 }
